50c7c24aef142dd4a85af960558c2433195bbd8b
Nathan Darrah <nmd001@lvc.edu>
2018-10-29 12:04:54 -0400

    Default Changelist

--- Client.cs
+++ Client.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace ConsoleApp1
 {
     public class Client
     {
+
+        public static void RunClient()
+        {
+            
+        }
         
     }
 }
--- FileGenerator.cs
+++ FileGenerator.cs
@@ -1,11 +1,32 @@
-﻿namespace ConsoleApp1
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
 {
     public class FileGenerator
     {
-        public static void main(string[] args)
+        private const int NUM_FILES = 200;
+        
+        /**
+         * our process to generate the files. First it picks a random size for
+         * the file we are on, and then picks a random number for each line
+         */
+        public static void run()
         {
-            
-            
+            Random gen = new Random();
+
+            for (int i = 1; i <= NUM_FILES; i++)
+            {
+                int fileSize = gen.Next(1_000, 10_000);
+
+                using (StreamWriter sw = File.CreateText("Files/" + i + ".txt"))
+                {
+                    for (int j = 0; j < fileSize; j++)
+                    {
+                        sw.WriteLine(gen.Next().ToString());
+                    }
+                }
+            }
         }
     }
 }
